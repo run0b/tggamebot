da59a7fe704ec103b85e3d38fed6833413ae4e61
Runo <Runo_0b@mail.ru>
2022-01-30 00:13:52 +0300

    BOT-4 Add Button

--- main.go
+++ main.go
@@ -31,10 +31,53 @@ func main() {
 	}
 
 	rK1Start := tb.ReplyButton{Text: "Начать"}
+	rK1Stat := tb.ReplyButton{Text: "Статистика"}
 	rK1Info := tb.ReplyButton{Text: "👀 Информация"}
 	rK1 := [][]tb.ReplyButton{
-		[]tb.ReplyButton{rK1Start, rK1Info},
+		[]tb.ReplyButton{rK1Start, rK1Info, rK1Stat},
 	}
+
+	rK2Stat := tb.ReplyButton{Text: "Руды добыто  ⛏ "}
+	rK2Kill := tb.ReplyButton{Text: "Убийства 🏹"}
+	rK2Xp := tb.ReplyButton{Text: "Навыки 💱"}
+	rK2Money := tb.ReplyButton{Text: "Балланс 💲"}
+	rK2back := tb.ReplyButton{Text: "Назад 🔙"}
+	rK2 := [][]tb.ReplyButton{
+		[]tb.ReplyButton{rK2Stat, rK2Kill, rK2Xp, rK2Money, rK2back},
+	}
+
+	rK3Rules := tb.ReplyButton{Text: "Правила "}
+	rK3Des := tb.ReplyButton{Text: "Описание"}
+	rK3Ore := tb.ReplyButton{Text: "Виды руд"}
+	rK3Chest := tb.ReplyButton{Text: "Сундуки"}
+	rK3back := tb.ReplyButton{Text: "Назад 🔙"}
+	rK3 := [][]tb.ReplyButton{
+		[]tb.ReplyButton{rK3Rules, rK3Des, rK3Ore, rK3Chest, rK3back},
+	}
+
+	b.Handle(&rK1Stat, func(m *tb.Message) {
+		b.Send(m.Sender, ""+m.Sender.FirstName+", Тут вы можете посмотреть общию статистику", &tb.ReplyMarkup{
+			ReplyKeyboard:       rK2,
+			ResizeReplyKeyboard: true,
+			// InlineKeyboard: inlineKeys,
+		})
+	})
+
+	b.Handle(&rK2back, func(m *tb.Message) {
+		b.Send(m.Sender, ""+m.Sender.FirstName+", Вы вернулись на главную страницу", &tb.ReplyMarkup{
+			ReplyKeyboard:       rK1,
+			ResizeReplyKeyboard: true,
+			// InlineKeyboard: inlineKeys,
+		})
+	})
+
+	b.Handle(&rK1Info, func(m *tb.Message) {
+		b.Send(m.Sender, ""+m.Sender.FirstName+", Тут вы можете ознакомится с механикой игры", &tb.ReplyMarkup{
+			ReplyKeyboard:       rK3,
+			ResizeReplyKeyboard: true,
+			// InlineKeyboard: inlineKeys,
+		})
+	})
 
 	b.Handle("/start", func(m *tb.Message) {
 		b.Send(m.Sender, m.Sender.FirstName+", Привет", &tb.ReplyMarkup{
